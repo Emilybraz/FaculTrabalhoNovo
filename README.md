4e115cb4f64b26078ca74911bd0d33f9ee7454b3
ph_braz <ph_braz@hotmail.com>
2021-05-24 18:58:41 +0100

    primeiro commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# todo-list WebApp
+#
+#
+# This Webapp is developed using nodejs and javascript wirh a NoSQL DB (MongoDB)
+# Path / for local and daily tasks 
+# Path /anything you want then it creats a list based on the url path lets se you want to create a work list
+# all you need to do is /work and that's it.
+#
+# app.js controls our calls
---
+++ app.js
@@ -0,0 +1,174 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+
+//mongoose for our nosql db 
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://phbraz:Patrycja0304@cluster0.95eou.mongodb.net/todoListDB", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+//Itens schema
+const itemSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        }
+    });
+
+//Model 
+const Item = mongoose.model("Item", itemSchema);
+
+//default items in our collection
+
+//muda isso aqui mano
+const items1 = new Item(
+    {
+        name: "Bem Vindo a lista de a fazeres"
+    });
+
+const items2 = new Item(
+    {
+        name: "Aperte o botao + para adicionar um novo item"
+    });
+
+const items3 = new Item(
+    {
+        name: "<-- Aperte esse botao para remover um item"
+    });
+
+const defaultItems = [items1, items2, items3]
+
+//list schema
+
+const listSchema = new mongoose.Schema(
+    {
+        name: String,
+        items: [itemSchema]
+    });
+    
+//model    
+const List = mongoose.model("List", listSchema);
+
+
+//home page localhost:3000/
+
+app.get("/", (req, res) => {
+
+    Item.find({}, (err, foundItems) => {
+
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+
+                } else {
+                    console.log("item padroes foram adicionados");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: "Hoje", newListItems: foundItems });
+        }
+    });
+
+});
+
+//any path 
+
+app.get("/:customListName", (req, res) => {
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customListName}, (err, foundList)=>{
+        if(!err){
+            if(!foundList){
+                //create a new list
+
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                res.redirect("/"+customListName);
+            }else{
+                
+                //show existing list
+                res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+            }
+        }
+
+    });
+
+});
+
+
+app.post("/", (req, res) => {
+    const itemName = req.body.newtodo;
+    const listName = req.body.list;
+
+    const newItemRequested = new Item(
+        {
+            name: itemName
+        });
+
+    if (listName === "Today") {
+
+        newItemRequested.save();
+        res.redirect("/");
+    }else{
+        List.findOne({name: listName}, (err, foundList)=>{
+            foundList.items.push(newItemRequested);
+            foundList.save();
+            res.redirect("/" + listName);
+        });
+    }
+});
+
+//delete items
+
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove({ _id: checkedItemId }, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
+        });
+
+    }else{
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, (err, foundList)=>{
+            if(!err){
+                res.redirect("/"+ listName);
+            }
+
+        });
+    }
+});
+
+//about page localhost:3000/about
+
+app.get("/about", (req, res) => {
+    res.render("about");
+
+});
+
+let port = process.env.PORT;
+if (port == null || port =="") {
+    port=3000;   
+}
+
+app.listen(port, () => {
+    console.log("Servidor iniciado com sucesso");
+});
